cd9cedb16b76343f07b3d5f872b6adc791a373d3
mjrb <www.w.com123@gmail.com>
2018-05-27 14:42:58 -0400

    add authenticated login reset and registration

--- src/calc_goggles/core.cljs
+++ src/calc_goggles/core.cljs
@@ -2,45 +2,90 @@
   (:require [reagent.core :as reagent :refer [atom]]
             [calc-goggles.create :refer [creator plot]]
             [calc-goggles.view :refer [model-viewer]]
+            [calc-goggles.stitch :as s]
+            [cljs.core.async :refer [ go >! <! chan alt! alts! pipe]]
+            [calc-goggles.utils :as utils :refer [feild-value label]]
             [calc-goggles.browse :refer [model-browser]]))
+
 (enable-console-print!)
-(defn lmao [] [:div "sss"])
+(defonce app-state (atom {:api-key "calcgoggles-qwpga"
+                          :content [model-browser]
+                          :client #js{}
+                          :logged-in false
+                          :shape #js{}
+                          }))
 
-(defn widget1 []
-  [:div.alert-primary "this is widget 1"])
-(defn widget2 []
-  [:div.alert-danger "this is widget 2"])
-
-(defonce app-state (atom {:text "Hello world!"}))
-(defonce shape (atom #js{}))
 (defn set-shape [new-shape]
-  (print @shape)
-  (reset! shape new-shape)
-  (print @shape))
-(defonce content (atom [creator set-shape]))
-(defn thr []
-  widget1)
+  (print (@app-state :shape))
+  (swap! app-state assoc :shape new-shape)
+  (print (@app-state :shape)))
+
 (defn on-js-reload [] )
 
+(defn no-auth-buttons []
+  [:span
+   [:input.btn.btn-primary
+    {:value "view" :type "button"
+     :on-click (fn [] (if (nil? (.-v (@app-state :shape) ))
+                        (js/alert "you must select a shape first")
+                        (swap! app-state assoc :content
+                               [model-viewer (@app-state :shape)]
+                               )))}]
+   [:input.btn.btn-primary
+    {:value "browse" :type "button"
+     :on-click #(swap! app-state assoc :content [model-browser])}]]
+  )
+(defn auth-buttons [logged-in?]
+  (if logged-in?
+    [:span
+     [:input.btn.btn-primary
+      {:value "create" :type "button"
+       :on-click #(swap! app-state assoc :content [creator set-shape])}]
+     [:input.btn.btn-primary
+      {:value "log out" :type "button"
+       :on-click #(do (swap! app-state assoc :content [model-browser])
+                      (swap! app-state assoc :logged-in false)
+                      ;;TODO why does this still give me auth id
+                      (swap! app-state (fn [state]
+                                         (.logout (state :client))
+                                         (print (.authedId (state :client)))
+                                         (print (.isAuthenticated (state :client)))
+                                         state)
+                             ))}]]
+    ;;else
+    [:span
+     [:input.btn.btn-primary
+     {:value "log in" :type "button"
+      :on-click #(swap! app-state assoc :content
+                         (reagent/as-element [utils/login-box app-state]))
+                  ;;(print @app-state)
+                  }]
+     [:input.btn.btn-primary
+      {:value "register" :type "button"
+       :on-click #(swap! app-state assoc :content
+                         [utils/register-box (@app-state :api-key)])}]
+     ]
+    ))
 
-(defn content-comp [cont]
-  (case cont
-    1 [widget1]
-    2 [widget2]
-    :br))
 (defn app []
   [:div
-  [:input.btn.btn-primary
-   {:value "create" :type "button"
-    :on-click #(reset! content [creator set-shape])}]
-  [:input.btn.btn-primary
-   {:value "view" :type "button"
-    :on-click #(reset! content [model-viewer @shape])}]
-   [:input.btn.btn-primary
-   {:value "browse" :type "button"
-    :on-click #(reset! content [model-browser])}]
-   @content
+   [no-auth-buttons]
+   [auth-buttons (@app-state :logged-in)]
+   (@app-state :content)
    ])
+
+(defn testcomp []
+  [:div
+   [:input.btn.btn-primary
+    {:value "log in" :type "button"
+     :on-click (fn [] (swap! app-state assoc :content
+                         [:div (reagent/as-element [utils/login-box app-state])]))
+     }]
+   (@app-state :content)]
+   )
+
+(defn comp1 []
+  [:h1 "test text"])
 
 (reagent/render-component
  [app]
--- src/calc_goggles/stitch.cljs
+++ src/calc_goggles/stitch.cljs
@@ -1,11 +1,17 @@
 (ns calc-goggles.stitch
-  (:require [cljs.core.async :refer [alt! go >! <! chan timeout put! take!]]))
-(defn prom->chan [prom]
-  (let [val-chan (chan)
-        err-chan (chan)]
-    (.then prom (fn [val] (go (>! val-chan val))))
-    (.catch prom (fn [err] (go (>! err-chan err))))
-    [val-chan err-chan]))
+  (:require [cljs.core.async :refer [ go >! <! chan pipe]]))
+(defn prom->chan
+  ([prom err-chan]
+   (let [val-chan (chan)]
+     (.then prom (fn [val] (go (>! val-chan val))))
+     (.catch prom (fn [err] (go (>! err-chan err))))
+     val-chan))
+  ([prom]
+   (let [val-chan (chan)
+         err-chan (chan)]
+     (.then prom (fn [val] (go (>! val-chan val))))
+     (.catch prom (fn [err] (go (>! err-chan err))))
+     [val-chan err-chan])))
 
 (defn get-clientp [api-key]
   (-> (.-stitch js/window)
@@ -14,3 +20,41 @@
 (defn get-client [api-key]
   (-> (get-clientp api-key)
       (prom->chan) ))
+(defn login
+  ([client user pass err-chan]
+  (prom->chan (.login client user pass) err-chan))
+  ([client user pass]
+  (prom->chan (.login client user pass))))
+(defn get-client-login [api-key username password]
+  (let [[client-chan err-chan] (get-client api-key)]
+    (go
+      (let [sclient (<! client-chan)
+            login-success (login sclient username password err-chan)]
+        (<! login-success)
+        (>! client-chan sclient)
+        ))
+    [client-chan err-chan]
+    ))
+(defn auth-provider [client type]
+  (-> (.-auth client)
+      (.provider type)))
+(defn email-reset-password [email api-key]
+  "sends password reset to email and returns channels for errors an success"
+  (let [[client-chan err-chan] (get-client api-key)
+        result-chan (chan)]
+    (go (let [success-chan (-> (<! client-chan)
+                                           (auth-provider "userpass")
+                                           (.sendPasswordReset email)
+                                           (prom->chan err-chan))]
+          ;;(go (js/alert (str "222: " (<! fail-chan))))
+          (pipe success-chan result-chan)))
+    [result-chan err-chan]))
+(defn register-email [username password api-key]
+  (let [[client-chan err-chan] (get-client api-key)]
+    (go (let [sclient (<! client-chan)
+              register-prom (.register sclient username password)
+              success-chan (prom->chan register-prom err-chan)]
+          (<! success-chan)
+          (>! client-chan sclient)
+          ))
+    [client-chan err-chan]))
