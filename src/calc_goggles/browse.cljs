071f9e27638fd8375eae1cc364337d4b88fafa7a
mjrb <www.w.com123@gmail.com>
2018-05-30 00:25:17 -0400

    add view button functionality

--- src/calc_goggles/browse.cljs
+++ src/calc_goggles/browse.cljs
@@ -1,5 +1,6 @@
 (ns calc-goggles.browse
   (:require [calc-goggles.stitch :as s]
+            [calc-goggles.view :refer [model-viewer]]
             [reagent.core :as reagent :refer [atom]]
             [cljs.core.async :as a]))
 (defonce objects (atom #js[] ))
@@ -24,20 +25,28 @@
     (if (empty? @objects)
       (do (update-objects app-state)
           )))
-(defn object-list-item [object]
+
+(defn view-object [id app-state]
+  (-> (s/atlas-db-coll (@app-state :client) (@app-state :db-name) "objects")
+      (.findOne #js{:_id id})
+;      (.execute)
+      (.then #(swap! app-state assoc :content (reagent/as-element [model-viewer %])))
+      (.catch #(js/alert %))
+      ))
+
+(defn object-list-item [object app-state]
   [:li (.-name object)
-   [:input {:type "button" :value "edit"
-            :on-click #(print (.-_id object))}]
     [:input {:type "button" :value "view"
-            :on-click #(print (.-_id object))}]
+            :on-click #(view-object (.-_id object) app-state)}]
    ])
+
 (defn model-browser [app-state]
   (possibly-update-objects app-state)
   (reagent/create-class
    {:display-name "model-browser"
     :reagent-render (fn [] (if (not-empty @objects)
                       (into [:ul] (map (fn [obj]
-                                         (reagent/as-element [object-list-item obj]))
+                                         (reagent/as-element [object-list-item obj app-state]))
                                          @objects))
                       [:ul [:li "loading"] [:li "objects"]]))
     :component-will-unmount #(reset! objects #js[]) }
