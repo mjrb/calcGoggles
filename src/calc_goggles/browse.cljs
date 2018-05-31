4eeed71b8da06b86fd3f0a8f1c44139c7f657e9e
mjrb <www.w.com123@gmail.com>
2018-05-31 01:36:41 -0400

    start to add search

--- src/calc_goggles/browse.cljs
+++ src/calc_goggles/browse.cljs
@@ -1,9 +1,11 @@
 (ns calc-goggles.browse
   (:require [calc-goggles.stitch :as s]
             [calc-goggles.view :refer [model-viewer]]
+            [calc-goggles.utils :refer [feild-value]]
             [reagent.core :as reagent :refer [atom]]
             [cljs.core.async :as a]))
-(defonce objects (atom #js[] ))
+(defonce objects (atom #js[]))
+(defonce all-objects (atom #js[]))
 (print (empty? @objects))
 (defonce re-poll (atom true))
 (defn reset []
@@ -15,8 +17,9 @@
           (s/atlas-db-coll (:client @app-state) ( :db-name @app-state) "objects")]
     (-> (.find objects-coll #js{} #js{:name true :_id true})
         (.execute)
-        (.then (fn [objs] (reset! objects objs)
-                 ;;i probably shouldn't do this, but ... idk
+        (.then (fn [objs]
+                 (reset! objects objs)
+                 (reset! all-objects objs)
                  (print @objects)
                  ))
         (.catch #(js/alert (str "model-browser querry error: " %)))
@@ -35,19 +38,35 @@
       ))
 
 (defn object-list-item [object app-state]
-  [:li (.-name object)
+  [:li 
     [:input {:type "button" :value "view"
-            :on-click #(view-object (.-_id object) app-state)}]
+             :on-click #(view-object (.-_id object) app-state)}]
+   (.-name object)
    ])
-
+(defn object-list [app-state]
+  (into [:ul]
+        (map (fn [obj]
+               (reagent/as-element [object-list-item obj app-state]))
+             @objects)))
+(defn contains [string other]
+  (some? (re-find (re-pattern (str "(?i)" string)) other)))
+;TODO impl username based search
+(defn obj-search []
+  (let [query (feild-value "query")]
+    (swap! objects filter (fn [obj] (contains query (.name obj))) @all-objects)))
 (defn model-browser [app-state]
   (possibly-update-objects app-state)
   (reagent/create-class
    {:display-name "model-browser"
     :reagent-render (fn [] (if (not-empty @objects)
-                      (into [:ul] (map (fn [obj]
-                                         (reagent/as-element [object-list-item obj app-state]))
-                                         @objects))
+                             [:div.container
+                              [:input.form-controls
+                               {:id "query"
+                                :default-value "object name"}]
+                              [:input.btn.btn-primary {:value "search!"
+                                                       :on-click obj-search}]
+                              (reagent/as-element [object-list app-state])
+                              ]
                       [:ul [:li "loading"] [:li "objects"]]))
     :component-will-unmount #(reset! objects #js[]) }
    ))
