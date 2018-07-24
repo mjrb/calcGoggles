09d63c37b184ea3e8ba998c7cf0d2acd7856c347
mjrb <www.w.com123@gmail.com>
2018-07-24 01:23:06 -0400

    refactor and use new util funcs

--- src/calc_goggles/core.cljs
+++ src/calc_goggles/core.cljs
@@ -4,8 +4,10 @@
             [calc-goggles.view :refer [model-viewer]]
             [calc-goggles.stitch :as s]
             [cljs.core.async :refer [ go >! <! chan alt! alts! pipe]]
-            [calc-goggles.utils :as utils :refer [feild-value label anon-login]]
-            [calc-goggles.browse :refer [model-browser view-object]]))
+            [calc-goggles.utils :as utils :refer [feild-value label]]
+            [calc-goggles.login :as login]
+            [calc-goggles.browse :refer [model-browser view-object]]
+            [calc-goggles.my-shapes :refer [my-shapes-browser]]))
 
 ;(enable-console-print!)
 ;; apikeys here are ok because this will be secured by an authorized orgin in stitch
@@ -16,16 +18,9 @@
                           :logged-in false
                           :db-name "test"
                           }))
-(anon-login app-state)
-(defn logout []
-  (swap! app-state assoc :content [model-browser])
-  (swap! app-state assoc :logged-in false)
-  ;;we only pretend to log out, because stitch doesn't really let us
-  (swap! app-state (fn [state]
-                     (.logout (state :client))
-                     (anon-login app-state)
-                     (assoc state :content (reagent/as-element [model-browser app-state]))
-                     )))
+(def set-content! (partial utils/set-content! app-state))
+(login/anon-login app-state)
+
 (defn send-shape [new-shape]
   (swap! app-state assoc :shape new-shape)
   (let [owner-id (.authedId (@app-state :client))
@@ -38,54 +33,48 @@
         (.then (fn [id-object] (view-object (.-_id id-object) app-state)))
         (.catch js/alert)
         )))
-(defn browse-button []
-  (if (not (.isAuthenticated (:client @app-state)))
-    (-> (s/get-clientp (@app-state :anon-api-key))
-        (.then (fn [client]
-                 (swap! app-state assoc :client client)
-                 (.login client)))
-        (.then (fn [id]
-                 (swap! app-state assoc :content
-                        (reagent/as-element [model-browser app-state]))))
-        (.catch (fn [err] (js/alert (str "failed to connect to calcGoggles."
-                             "please try to refresh page to reconnect. "
-                             err))))
-        )
-  (swap! app-state assoc :content
-         (reagent/as-element [model-browser app-state]))))
 
-(defn on-js-reload [] )
+(defn wrap-auth-check [func]
+  (fn []
+    (if (not (.isAuthenticated (:client @app-state)))
+      (-> (s/get-clientp (@app-state :anon-api-key))
+          (.then (fn [client]
+                   (swap! app-state assoc :client client)
+                   (.login client)))
+          (.then (fn [id]
+                   (func)
+                   (.catch (fn [err]
+                             (js/alert (str "failed to connect to calcGoggles."
+                                            "please try to refresh page to reconnect. "
+                                            err)))))))
+      (func))))
+
+(defn on-js-reload [])
 
 (defn no-auth-buttons []
   [:span.btn-group {:style {:margin-right -4}}
-   [:input.btn.btn-secondary {:value "calcGoggles" :type "button"}]
-   [:input.btn.btn-primary
-    {:value "browse" :type "button"
-     :on-click browse-button
-     }]]
-  )
+   (utils/boot-btn-secondary "calcGoggles" #())
+   (utils/boot-btn-primary
+    "browse"
+    (wrap-auth-check #(set-content! [model-browser app-state])))])
+
 (defn auth-buttons [logged-in?]
   (if logged-in?
     [:span.btn-group
-     [:input.btn.btn-primary
-      {:value "create" :type "button"
-       :on-click #(swap! app-state assoc :content [creator send-shape])}]
-     [:input.btn.btn-primary
-      {:value "log out" :type "button"
-       :on-click logout }]]
+     (utils/boot-btn-primary "create"
+                             #(set-content! [creator send-shape]))
+     (utils/boot-btn-primary
+      "my shapes"
+      (wrap-auth-check #(set-content! [my-shapes-browser app-state])))
+     (utils/boot-btn-primary "log out" #(login/logout app-state))]
     ;;else
     [:span.btn-group
-     [:input.btn.btn-primary
-     {:value "log in" :type "button"
-      :on-click #(swap! app-state assoc :content
-                         (reagent/as-element [utils/login-box app-state]))
-                  ;;(print @app-state)
-                  }]
-     [:input.btn.btn-primary
-      {:value "register" :type "button"
-       :on-click #(swap! app-state assoc :content
-                         [utils/register-box (@app-state :api-key)])}]
-     ]
+     (utils/boot-btn-primary "log in"
+                             #(set-content! [login/login-box app-state]))
+     (utils/boot-btn-primary
+      "register"
+      #(set-content!
+        [login/register-box (@app-state :api-key)]))]
     ))
 
 (defn app []
